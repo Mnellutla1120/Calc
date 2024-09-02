8d09bc42f6dc471b42b0fadd6c8afeec0402d1cf
Mnellutla1120 <162918815+Mnellutla1120@users.noreply.github.com>
2024-09-02 14:31:10 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,24 @@
 
+//CALC PROGRAM
+const display = document.getElementById("display");
 
-function operation (a,b){
+
+function appendToDisplay(input){
+display.value += input;
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
 
 }
